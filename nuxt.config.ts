c71fa5afd4056158633a5dc2429017660de32297
Kevin Dyes <kevin.dyes@siemens.com>
2025-03-07 09:05:27 +0100

    feat: add openapi spec for routes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,10 +5,30 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   plugins: [],
-  css: ['~/assets/css/main.css'],
+  css: ["~/assets/css/main.css"],
   vite: {
-    plugins: [
-      tailwindcss()
-    ]
-  }
+    plugins: [tailwindcss()],
+  },
+  nitro: {
+    experimental: {
+      openAPI: true
+    },
+    openAPI: {
+      production: false,
+      meta: {
+        title: 'Gameday Helperplan',
+        description: 'A little tool to create, manage and distribute helper plans for sports teams.',
+        version: '0.1'
+      },
+      route: "/_docs/openapi.json",
+      ui: {
+        scalar: {
+          route: "/_docs/scalar",
+        },
+        swagger: {
+          route: "/_docs/swagger",
+        },
+      },
+    },
+  },
 });
