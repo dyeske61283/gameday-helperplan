f20b1d4da983acf53b62d67f79984d291e12b302
Kevin Dyes <kevin.dyes@siemens.com>
2025-03-07 09:04:08 +0100

    feat: add error boundary page

---
+++ error.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="min-h-screen flex items-center justify-center bg-base-200">
+        <div class="text-center">
+            <div class="flex justify-center mb-8">
+                <AlertCircle :size="32" />
+            </div>
+            <h1 class="text-5xl font-bold mb-4">404</h1>
+            <p class="text-xl mb-8">Hoppla, verlaufen?</p>
+            <NuxtLink to="/" class="btn btn-primary">
+                <Home class="" />
+                Geh ham!
+            </NuxtLink>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { Home, AlertCircle } from 'lucide-vue-next';
+defineProps({
+    error: Object
+})
+</script>
