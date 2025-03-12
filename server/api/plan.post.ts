3d7c26be2f143d98a7b154cefed19a05a6751ba9
Kevin Dyes <kevin.dyes@icloud.com>
2025-03-12 23:38:03 +0100

    feat: move api routes and implement post

--- server/api/plan.post.ts
+++ server/api/plan.post.ts
@@ -1,14 +1,62 @@
-export default defineEventHandler(async (event) => {
-  try {
-    const body = await readValidatedBody(event, (body) => {
-      throw new Error("Hihi, you are dumb");
-    })
+import { z } from "zod";
+import { v7 as uuidv7 } from "uuid";
 
-  }
-  catch(error) {
+defineRouteMeta({
+  openAPI: {
+    tags: ["Plan"],
+    operationId: "postplan",
+    description: "Create a new plan.",
+    requestBody: {
+      description:
+        "If provided, the password will be used to encrypt the plan.",
+      required: true,
+      content: {
+        "application/json": {
+          examples: {
+            "Plan without password": {
+              "value": {
+                "name": "Season 25/26 Handball"
+              }
+            },
+            "Plan with password": {
+              value: {
+                "name": "Season 1999 Volleyball Women Location 5",
+                "password": "ABCDEFG12345"
+              }
+            }
+          },
+          schema: {
+            type: "object",
+            required: ["name"],
+            properties: {
+              name: {
+                type: "string",
+              },
+              password: {
+                type: "string",
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+});
+
+export default defineEventHandler(async (event) => {
+  const postPlanRequestSchema = z.object({
+    name: z.string().trim().min(3),
+    password: z.string().optional()
+  });
+  try {
+    const body = await readValidatedBody(event, postPlanRequestSchema.parse);
+  } catch (error) {
     if (error instanceof Error) {
+      setResponseStatus(event, 400);
       return error.message;
     }
   }
-  return 'Hello Nitro'
-})
+  const planId = uuidv7();
+  setResponseHeader(event, "location", `/api/plan/${planId}`);
+  return sendNoContent(event, 201)
+});
