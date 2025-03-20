9057728270211c11d013e4d8336dc5a0f87270a4
Kevin Dyes <kevin.dyes@siemens.com>
2025-03-20 19:34:38 +0100

    feat: add link to github project

--- app.vue
+++ app.vue
@@ -2,9 +2,10 @@
   <div class="min-h-screen bg-base-200 flex flex-col">
     <div class="navbar bg-base-100 shadow-lg">
       <div class="flex-1 flex gap-1 justify-center items-center">
-        <span class="btn btn-ghost text-xl disabled">Helferplan</span>
+        <span class="text-xl">Helferplan</span>
         <button :class="{invisible: !planId}" class="btn btn-accent btn-sm ml-auto">Plan anzeigen</button>
         <button :class="{invisible: !planId}" class="btn btn-accent btn-sm"><Share :size="18"></Share></button>
+        <NuxtLink to="https://github.com/dyeske61283/gameday-helperplan" target="_blank" class="btn btn-ghost btn-sm"><GitHubIcon></GitHubIcon></NuxtLink>
       </div>
     </div>
     <NuxtPage />
@@ -14,5 +15,7 @@
 
 <script setup lang="ts">
   import { Share } from 'lucide-vue-next';
+  import { GitHubIcon } from 'vue3-simple-icons';
+
   const planId = usePlanId();
 </script>
