302463401b1c803f0c53f490f2a2279cd269d43a
Kevin Dyes <kevin.dyes@siemens.com>
2025-03-20 20:10:42 +0100

    feat: basic helper types input

---
+++ pages/helper-types.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container mx-auto p-4 flex flex-col items-center justify-center flex-1">
+    <span class="text-xl">Welche Arten von Helfern brauchst du?</span>
+    <span class="text-sm font-extralight mb-4">Das kannst du später auch noch anpassen</span>
+    <input type="text" class="input w-80 m-4" placeholder="Koch, Ballonflechter, Schiedsrichter, ..." name="" id="">
+    <div class="skeleton h-80 w-80 text-center">
+      List of Helper Types...
+    </div>
+    <button class="btn btn-soft mt-auto">Überspringen</button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
