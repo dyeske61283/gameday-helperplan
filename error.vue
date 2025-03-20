b5b0565d9a789d7e59f34fb8d20cec1c8768989c
Kevin Dyes <kevin.dyes@siemens.com>
2025-03-20 19:34:06 +0100

    fix: weird icon placing on error page

--- error.vue
+++ error.vue
@@ -1,10 +1,9 @@
 <template>
     <div class="min-h-screen flex items-center justify-center bg-base-200">
         <div class="text-center">
-            <div class="flex justify-center mb-8">
-                <AlertCircle :size="32" />
-            </div>
-            <h1 class="text-5xl font-bold mb-4">404</h1>
+            <h1 class="text-5xl font-bold mb-4 flex items-center justify-center">
+                <AlertCircle class="mr-4" :size="32" /> 404
+            </h1>
             <p class="text-xl mb-8">Hoppla, verlaufen?</p>
             <NuxtLink to="/" class="btn btn-primary">
                 <Home class="" />
