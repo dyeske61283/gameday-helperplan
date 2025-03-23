99b3bd8b2816178f6f02bf7932788e52dc1b549a
Kevin Dyes <kevin.dyes@icloud.com>
2025-03-23 23:52:36 +0100

    feat: add auto animate into nuxt modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,9 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   plugins: [],
+  modules: [
+    "@formkit/auto-animate/nuxt"
+  ],
   css: ["~/assets/css/main.css"],
   vite: {
     plugins: [tailwindcss()],
