3a075e612da509d7cb88d5a1514b79c2b7dc385c
Kevin Dyes <kevin.dyes@icloud.com>
2025-03-23 23:52:57 +0100

    feat: flesh out helper types page

--- pages/helper-types.vue
+++ pages/helper-types.vue
@@ -2,16 +2,25 @@
   <div class="container mx-auto p-4 flex flex-col items-center justify-center flex-1">
     <span class="text-xl">Welche Arten von Helfern brauchst du?</span>
     <span class="text-sm font-extralight mb-4">Das kannst du später auch noch anpassen</span>
-    <input type="text" class="input w-80 m-4" placeholder="Koch, Ballonflechter, Schiedsrichter, ..." name="" id="">
-    <div class="skeleton h-80 w-80 text-center">
-      List of Helper Types...
+    <div class="flex flex-row w-80 m-4">
+      <input type="text" class="input" placeholder="Koch, Ballonflechter, Schiedsrichter, ..." name="New helper type text input" id="new-helper-type-input" v-model="helperTypesInput" @keyup.enter="addHelperType">
+      <button class="btn btn-primary ml-2" @click="addHelperType()">+</button>
     </div>
-    <button class="btn btn-soft mt-auto">Überspringen</button>
+    <ul class="list flew-grow max-h-90 w-80 text-center mt-4 bg-base-100 rounded-box shadow-md" v-auto-animate>
+      <li class="list-row" v-for="(item, index) in helperTypes" :key="index">{{item}}</li>
+    </ul>
+    <NuxtLink to="" class="btn btn-soft mt-auto">Überspringen</NuxtLink>
   </div>
 </template>
 
 <script lang="ts" setup>
+  const helperTypes = ref<string[]>([]);
+  const helperTypesInput = ref<string>('');
 
+  const addHelperType = () => {
+    helperTypes.value.push(helperTypesInput.value);
+    helperTypesInput.value = '';
+  };
 </script>
 
 <style>
