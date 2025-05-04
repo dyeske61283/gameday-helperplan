080dd36b17e6f16881352518d2d8cad2034c9ec8
Kevin Dyes <kevin.dyes@icloud.com>
2025-05-04 23:46:06 +0200

    feat: add even details page including view transition

---
+++ app/pages/plans/[id]/events/[eventId]/index.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+
+  definePageMeta({
+    pageTransition: {
+      name: '',
+      mode: 'default'
+    },
+  });
+
+  const eventId = useRoute().params.eventId;
+</script>
+
+<template>
+  <h2>
+    Hello from eventId: {{ eventId }}
+  </h2>
+</template>
+
+<style scoped>
+h2 {
+    view-transition-name: event-title;
+  }
+</style>
