6ec5cce74743a5c18fd0a45da44696643e1161af
Kevin Dyes <kevin.dyes@icloud.com>
2025-05-05 23:46:01 +0200

    feat: redo top menu

--- app/app.vue
+++ app/app.vue
@@ -5,9 +5,21 @@
         <span class="text-xl font-bold">Helferplan</span>
       </div>
       <div class="flex-none">
-        <NuxtLink to="https://github.com/dyeske61283/gameday-helperplan" target="_blank" class="btn btn-ghost btn-sm">
-          <GitHubIcon></GitHubIcon>
-        </NuxtLink>
+        <div class="dropdown dropdown-bottom dropdown-end">
+          <button tabindex="0" class="btn btn-primary btn-soft">
+            <Menu></Menu>
+          </button>
+          <ul tabindex="0" class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+            <li><NuxtLink>Neuen Plan erstellen</NuxtLink></li>
+            <li><NuxtLink>Anderen Plan auswählen</NuxtLink></li>
+            <li><NuxtLink>Impressum</NuxtLink></li>
+            <li>
+              <NuxtLink to="https://github.com/dyeske61283/gameday-helperplan" target="_blank">
+                Open Source auf <GitHubIcon></GitHubIcon>
+              </NuxtLink>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
     <div class="flex-1">
@@ -18,6 +30,7 @@
 
 <script setup lang="ts">
 import { GitHubIcon } from 'vue3-simple-icons';
+import { Menu } from 'lucide-vue-next';
 
 const planId = usePlanId();
 </script>
