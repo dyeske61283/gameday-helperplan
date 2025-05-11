fb6f405ed46e78dd24cee0fea507181bf79cbe70
Kevin Dyes <kevin.dyes@icloud.com>
2025-05-11 15:34:44 +0200

    feat: add toggle button to switch views between events and helpers
    and also add some links in the top menu.

--- app/app.vue
+++ app/app.vue
@@ -5,14 +5,32 @@
         <span class="text-xl font-bold">Helferplan</span>
       </div>
       <div class="flex-none">
+        <label class="btn btn-soft swap mr-2">
+          <!-- this hidden checkbox controls the state -->
+          <input type="checkbox" />
+          <Calendar class="swap-off"></Calendar>
+          <Users class="swap-on"></Users>
+        </label>
         <div class="dropdown dropdown-bottom dropdown-end">
           <button tabindex="0" class="btn btn-primary btn-soft">
             <Menu></Menu>
           </button>
           <ul tabindex="0" class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
-            <li><NuxtLink>Neuen Plan erstellen</NuxtLink></li>
-            <li><NuxtLink>Anderen Plan auswählen</NuxtLink></li>
-            <li><NuxtLink>Impressum</NuxtLink></li>
+            <li>
+              <NuxtLink>Neuen Plan erstellen</NuxtLink>
+            </li>
+            <li>
+              <NuxtLink>Anderen Plan auswählen</NuxtLink>
+            </li>
+            <li>
+              <NuxtLink>Beispiel Plan</NuxtLink>
+            </li>
+            <li>
+              <NuxtLink>Wie's funktioniert?</NuxtLink>
+            </li>
+            <li>
+              <NuxtLink>Impressum</NuxtLink>
+            </li>
             <li>
               <NuxtLink to="https://github.com/dyeske61283/gameday-helperplan" target="_blank">
                 Open Source auf <GitHubIcon></GitHubIcon>
@@ -30,7 +48,7 @@
 
 <script setup lang="ts">
 import { GitHubIcon } from 'vue3-simple-icons';
-import { Menu } from 'lucide-vue-next';
+import { Calendar, Menu, Users } from 'lucide-vue-next';
 
 const planId = usePlanId();
 </script>
